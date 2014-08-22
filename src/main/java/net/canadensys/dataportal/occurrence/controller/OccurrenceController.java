d8b7778432f4e10b83795470dd293de0ac267d9e
Pedro Guimarães <tigreped@gmail.com>
2014-08-22 08:54:07 -0300

    Cleaning unused imports

--- src/main/java/net/canadensys/dataportal/occurrence/controller/OccurrenceController.java
+++ src/main/java/net/canadensys/dataportal/occurrence/controller/OccurrenceController.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import net.canadensys.dataportal.occurrence.OccurrenceService;
